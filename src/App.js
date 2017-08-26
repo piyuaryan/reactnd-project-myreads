1e9feae769474248368b969bbf725b18d57c5e55
piyu.aryan@gmail.com <piyu.aryan@gmail.com>
2017-08-26 16:49:40 +1000

    feat: Created component for SearchPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import {Link, Route} from "react-router-dom";
 import * as BooksAPI from "./utils/BooksAPI";
 import BookList from "./components/BookList";
+import BookSearch from "./components/BookSearch";
 
 class BooksApp extends Component {
 
@@ -28,27 +29,8 @@ class BooksApp extends Component {
         return (
             <div className="app">
 
-                <Route path="/create" render={() => (
-                    <div className="search-books">
-                        <div className="search-books-bar">
-                            {/*<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>*/}
-                            <div className="search-books-input-wrapper">
-                                {/*
-                                 NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                                 You can find these search terms here:
-                                 https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                                 However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                                 you don't find a specific author or title. Every search is limited by search terms.
-                                 */}
-                                <input type="text" placeholder="Search by title or author"/>
-
-                            </div>
-                        </div>
-                        <div className="search-books-results">
-                            <ol className="books-grid"></ol>
-                        </div>
-                    </div>
+                <Route path="/create" render={({history}) => (
+                    <BookSearch/>
                 )}/>
 
                 <Route exact path="/" render={() => (
